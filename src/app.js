bcb5faf1db4eaeab71212ca484bd65c5ca033239
Abhishek5723 <104137737+Abhishek5723@users.noreply.github.com>
2024-10-25 01:05:38 +0530

    created a server

---
+++ src/app.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const app = express();
+
+// Route for the root path
+app.get("/", (req, res) => {
+    res.send("Namaste from the Server");
+});
+
+// Route for "/hello"
+app.get("/hello", (req, res) => {
+    res.send("Hello hello helloooo");
+});
+
+// Route for "/rest"
+app.get("/rest", (req, res) => {
+    res.send("Hello from the Server");
+});
+
+// Start the server
+app.listen(8000, () => {
+    console.log("Server running on port 8000");
+});
