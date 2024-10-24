84646ccc9a43a816934dbc34d03bfc381128c6d0
Abhishek5723 <104137737+Abhishek5723@users.noreply.github.com>
2024-10-25 02:07:56 +0530

    exploring get post

--- src/app.js
+++ src/app.js
@@ -16,6 +16,9 @@ app.get("/rest", (req, res) => {
     res.send("Hello from the Server");
 });
 
+app.post("/pello", (req, res) => {
+    res.send("pello pello pelloooo");
+});
 // Start the server
 app.listen(8000, () => {
     console.log("Server running on port 8000");
